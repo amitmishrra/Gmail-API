72faad9e98ad8fbc94124121df38dcc7ba09c870
gopalkrish1 <g.krishna@kaliberlabs.com>
2024-10-01 03:59:42 +0530

    'init'

---
+++ Gmail API/GetMails.cs
@@ -0,0 +1,74 @@
+﻿using GmailAPI.APIHelper;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Gmail_API
+{
+    [TestClass]
+    public class GetMails
+    {
+        [TestMethod]
+        public void GetMail()
+        {
+            string mail = ConfigurationManager.AppSettings["HostAddress"];
+            GmailService GmailService = GmailAPIHelper.GetService();
+            List<Gmail> EmailList = new List<Gmail>();
+            UsersResource.MessagesResource.ListRequest ListRequest = GmailService.Users.Messages.List(mail);
+            ListRequest.LabelIds = "INBOX";
+            ListRequest.IncludeSpamTrash = false;
+            ListRequest.Q = "is:unread";
+
+            //GET ALL EMAILS
+            ListMessagesResponse ListResponse = ListRequest.Execute();
+
+            Console.WriteLine("Length : " + ListResponse.Messages.Count);
+            foreach (Message Msg in ListResponse.Messages)
+            {
+                UsersResource.MessagesResource.GetRequest Message = GmailService.Users.Messages.Get(mail, Msg.Id);
+                Message MsgContent = Message.Execute();
+
+                string MailBody = string.Empty;
+
+                if (MsgContent.Payload.Parts == null && MsgContent.Payload.Body != null)
+                {
+                    // If there are no parts, get the body data directly
+                    MailBody = MsgContent.Payload.Body.Data;
+                }
+                else if (MsgContent.Payload.Parts != null)
+                {
+                    // Extract the data from the parts
+                    MailBody = GmailAPIHelper.MsgNestedParts(MsgContent.Payload.Parts);
+                }
+
+                // Decode the base64-encoded body
+                string ReadableText = DecodeBase64String(MailBody);
+                Console.WriteLine(ReadableText);
+            }
+        }
+
+        // Decode the Base64 string
+        private string DecodeBase64String(string base64Input)
+        {
+            if (string.IsNullOrEmpty(base64Input))
+                return string.Empty;
+
+            // Gmail API Base64 URL encoding replacements
+            base64Input = base64Input.Replace("-", "+").Replace("_", "/");
+
+            // Ensure the string length is a multiple of 4
+            switch (base64Input.Length % 4)
+            {
+                case 2: base64Input += "=="; break;
+                case 3: base64Input += "="; break;
+            }
+
+            byte[] base64EncodedBytes = Convert.FromBase64String(base64Input);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
---
+++ Gmail API/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gmail API")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gmail API")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe80958b-6f44-4a12-963e-c94646301421")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
